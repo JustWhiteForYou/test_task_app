2a23db6030bdceb9dcec2ef548e15a34316feaff
JustWhiteForYou <zijrfe2@gmail.com>
2023-05-22 19:35:33 +0300

    refactor: Rename method

--- lib/domain/color_changer.dart
+++ lib/domain/color_changer.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-Color changeBackgroundColor() {
+Color randomColor() {
   return Color.fromRGBO(
     Random().nextInt(256),
     Random().nextInt(256),
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -35,7 +35,7 @@ class _MainScreenState extends State<MainScreen> {
         onTap: () {
           setState(
             () {
-              backgroundColor = changeBackgroundColor();
+              backgroundColor = randomColor();
             },
           );
         },
