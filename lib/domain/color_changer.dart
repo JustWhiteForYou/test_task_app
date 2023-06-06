10e598b6bd2336f540b67020056f5e19046d4c35
JustWhiteForYou <zijrfe2@gmail.com>
2023-06-06 18:17:16 +0300

    feature: Add random generation for opacity

--- lib/domain/color_changer.dart
+++ lib/domain/color_changer.dart
@@ -2,11 +2,15 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-Color randomColor() {
-  return Color.fromRGBO(
-    Random().nextInt(256),
-    Random().nextInt(256),
-    Random().nextInt(256),
-    1,
-  );
+///Generate random color from RGB
+class ColorChanger{
+  static Color randomColor() {
+    return Color.fromRGBO(
+      Random().nextInt(256),
+      Random().nextInt(256),
+      Random().nextInt(256),
+      Random().nextInt(101).toDouble()/100, //Забув що можна ділити все це.
+    );
+  }
 }
+
